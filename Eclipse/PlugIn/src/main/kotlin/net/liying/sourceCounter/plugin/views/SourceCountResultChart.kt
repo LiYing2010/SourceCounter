dc22fa3194a1300a8b0db3bea676e5ca67d54178
LiYing <liying.cn.2010@gmail.com>
2016-05-31 16:25:37 +0900

    add feature: count by selected resource

--- Eclipse/PlugIn/src/main/kotlin/net/liying/sourceCounter/plugin/views/SourceCountResultChart.kt
+++ Eclipse/PlugIn/src/main/kotlin/net/liying/sourceCounter/plugin/views/SourceCountResultChart.kt
@@ -1,6 +1,7 @@
 package net.liying.sourceCounter.plugin.views
 
 import org.eclipse.swt.*
+import org.eclipse.swt.events.SelectionEvent
 import org.eclipse.swt.widgets.*
 
 import org.eclipse.core.resources.*
@@ -13,12 +14,12 @@ import net.liying.sourceCounter.plugin.FileCountResult
 class SourceCountResultChart(parent: Composite, style: Int): BaseSourceCountResultChart(parent, style) {
 	private var resultList = mutableListOf<FileCountResult>()
 
-	private val treeItemMap = mutableMapOf<IResource, TreeItem>()
+	private val treeItemMap = mutableMapOf<IContainer, TreeItem>()
 
-	val imageProject = ResourceManager.getPluginImageDescriptor(
+	private val imageProject = ResourceManager.getPluginImageDescriptor(
 			"SourceCounter", "images/icons/project.gif").createImage()
 
-	val imageFolder = ResourceManager.getPluginImageDescriptor(
+	private val imageFolder = ResourceManager.getPluginImageDescriptor(
 			"SourceCounter", "images/icons/folder.gif").createImage()
 
 	// =========================================================================
@@ -26,8 +27,14 @@ class SourceCountResultChart(parent: Composite, style: Int): BaseSourceCountResu
 	fun showResult(resultList: List<FileCountResult>) {
 		this.resultList = resultList.toMutableList()
 
+		this.resultList.sortBy {
+			result -> result.file.fullPath.toString()
+		}
+
 		this.displayResultTree()
 	}
+
+	// =========================================================================
 
 	private fun displayResultTree() {
 		this.treeItemMap.clear()
@@ -35,20 +42,17 @@ class SourceCountResultChart(parent: Composite, style: Int): BaseSourceCountResu
 		this.tree.removeAll()
 
 		this.resultList.forEach {
-			result ->
-				val treeItem = this.createTreeItem(result.file.getParent())
-
-				treeItem.data = result
+			result -> this.createTreeItem(result.file.getParent())
 		}
 	}
 
-	private fun createTreeItem(resource: IResource): TreeItem {
-		var treeItem = this.treeItemMap.get(resource)
+	private fun createTreeItem(container: IContainer): TreeItem {
+		var treeItem = this.treeItemMap.get(container)
 		if (treeItem != null) {
 			return treeItem
 		}
 
-		val parent = resource.parent
+		val parent = container.parent
 		treeItem =
 			if (parent != null && !(parent is IWorkspaceRoot)) {
 				val parentItem = this.createTreeItem(parent)
@@ -57,15 +61,92 @@ class SourceCountResultChart(parent: Composite, style: Int): BaseSourceCountResu
 				TreeItem(this.tree, SWT.NONE)
 			}
 
-		treeItem.setText(resource.name)
+		treeItem.data = container
 
-		when (resource) {
+		treeItem.setText(container.name)
+
+		when (container) {
 			is IProject -> treeItem.setImage(0, this.imageProject)
 			is IFolder -> treeItem.setImage(0, this.imageFolder)
 		}
 
-		this.treeItemMap.put(resource, treeItem)
+		this.treeItemMap.put(container, treeItem)
 
 		return treeItem
 	}
+
+	override fun treeSelectionListener(e: SelectionEvent) {
+		val treeItem = this.tree.selection.getOrNull(0)
+
+		if (treeItem == null) {
+			return
+		}
+
+		this.countByTreeItem(treeItem)
+	}
+
+	private fun countByTreeItem(treeItem: TreeItem) {
+		this.countByPath(treeItem)
+		this.countByType(treeItem)
+	}
+
+	private fun countByPath(treeItem: TreeItem) {
+		treeItem.items.forEach {
+			childItem ->
+				val childResource = childItem.data as IContainer
+				val descendantResultList = this.allDescendantResultList(childResource)
+				val statementSum = this.statementSum(descendantResultList)
+
+				// TODO
+				println("Path: ${childItem.text}: ${statementSum}")
+		}
+
+		val resource = treeItem.data as IContainer
+		val descendantResultList = this.directDescendantResultList(resource)
+		val statementSum = this.statementSum(descendantResultList)
+
+		// TODO
+		println("Path: .: ${statementSum}")
+	}
+
+	private fun countByType(treeItem: TreeItem) {
+		val resource = treeItem.data as IContainer
+		val descendantResultList = this.allDescendantResultList(resource)
+
+		val keySelector = {
+			result: FileCountResult -> result.countResult.type
+		}
+		val valueTransform = {
+			result: FileCountResult -> result.countResult.statement
+		}
+		val groupedList = descendantResultList.groupBy(keySelector, valueTransform)
+		val groupedSum = groupedList.mapValues {
+			entry -> entry.value.sum()
+		}
+
+		// TODO
+		groupedSum.forEach {
+			entry -> println("${entry.key}: ${entry.value}")
+		}
+	}
+
+	private fun isAncestor(descendant: IResource, ancestor: IContainer): Boolean
+		= when (descendant.parent) {
+				null -> false
+				ancestor -> true
+				else -> this.isAncestor(descendant.parent, ancestor)
+			}
+
+	private fun allDescendantResultList(ancestor: IContainer): List<FileCountResult>
+		= this.resultList.filter {
+				result -> this.isAncestor(result.file, ancestor)
+			}
+
+	private fun directDescendantResultList(ancestor: IContainer): List<FileCountResult>
+		= this.resultList.filter {
+				result -> result.file.parent == ancestor
+			}
+
+	private fun statementSum(resultList: List<FileCountResult>): Int
+		= resultList.sumBy { result -> result.countResult.statement }
 }
--- Eclipse/PlugIn/src/main/kotlin/net/liying/sourceCounter/plugin/views/base/BaseSourceCountResultChart.java
+++ Eclipse/PlugIn/src/main/kotlin/net/liying/sourceCounter/plugin/views/base/BaseSourceCountResultChart.java
@@ -6,9 +6,15 @@ import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 
 public class BaseSourceCountResultChart extends Composite {
 	protected Tree tree;
+
+	protected void treeSelectionListener(SelectionEvent e) {
+		// do nothing here, implemented in sub-class
+	}
 
 	/**
 	 * Create the composite.
@@ -21,13 +27,18 @@ public class BaseSourceCountResultChart extends Composite {
 		setLayout(new FormLayout());
 
 		tree = new Tree(this, SWT.BORDER);
+		tree.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				BaseSourceCountResultChart.this.treeSelectionListener(e);
+			}
+		});
 		FormData fd_tree = new FormData();
 		fd_tree.bottom = new FormAttachment(100, -4);
 		fd_tree.right = new FormAttachment(100, -4);
 		fd_tree.top = new FormAttachment(0, 4);
 		fd_tree.left = new FormAttachment(0, 4);
 		tree.setLayoutData(fd_tree);
-
 	}
 
 	@Override
