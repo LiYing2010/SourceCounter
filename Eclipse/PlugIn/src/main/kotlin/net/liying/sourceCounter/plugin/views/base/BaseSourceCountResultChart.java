33de46dc334f05c1f73a7c81ae78b0bcc9555a83
LiYing <liying.cn.2010@gmail.com>
2016-06-01 11:33:10 +0900

    separate charts into different tabs

--- Eclipse/PlugIn/src/main/kotlin/net/liying/sourceCounter/plugin/views/base/BaseSourceCountResultChart.java
+++ Eclipse/PlugIn/src/main/kotlin/net/liying/sourceCounter/plugin/views/base/BaseSourceCountResultChart.java
@@ -9,11 +9,17 @@ import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Tree;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.custom.CTabFolder;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.custom.CTabItem;
 
 public class BaseSourceCountResultChart extends Composite {
 	protected Tree tree;
 	protected Composite chartDisplay_CountByPath;
 	protected Composite chartDisplay_CountByType;
+	private CTabFolder tabFolder;
+	private CTabItem tbtmNewItem2;
+	private CTabItem tbtmNewItem1;
 
 	protected void treeSelectionListener(SelectionEvent e) {
 		// do nothing here, implemented in sub-class
@@ -43,23 +49,29 @@ public class BaseSourceCountResultChart extends Composite {
 		fd_tree.left = new FormAttachment(0, 4);
 		tree.setLayoutData(fd_tree);
 
-		chartDisplay_CountByPath = new Composite(this, SWT.EMBEDDED);
-		chartDisplay_CountByPath.setLayout(new GridLayout(1, false));
-		FormData fd_chartDisplay_CountByPath = new FormData();
-		fd_chartDisplay_CountByPath.left = new FormAttachment(tree, 8);
-		fd_chartDisplay_CountByPath.right = new FormAttachment(100, -8);
-		fd_chartDisplay_CountByPath.top = new FormAttachment(0, 8);
-		fd_chartDisplay_CountByPath.bottom = new FormAttachment(50, -4);
-		chartDisplay_CountByPath.setLayoutData(fd_chartDisplay_CountByPath);
+		tabFolder = new CTabFolder(this, SWT.BORDER);
+		FormData fd_tabFolder = new FormData();
+		fd_tabFolder.left = new FormAttachment(tree, 8);
+		fd_tabFolder.bottom = new FormAttachment(100, -4);
+		fd_tabFolder.right = new FormAttachment(100, -8);
+		fd_tabFolder.top = new FormAttachment(0, 4);
+		tabFolder.setLayoutData(fd_tabFolder);
+		tabFolder.setSelectionBackground(
+				Display.getCurrent().getSystemColor(SWT.COLOR_TITLE_INACTIVE_BACKGROUND_GRADIENT));
 
-		chartDisplay_CountByType = new Composite(this, SWT.EMBEDDED);
+		tbtmNewItem1 = new CTabItem(tabFolder, SWT.NONE);
+		tbtmNewItem1.setText("Count by Type");
+
+		chartDisplay_CountByType = new Composite(tabFolder, SWT.EMBEDDED);
+		tbtmNewItem1.setControl(chartDisplay_CountByType);
 		chartDisplay_CountByType.setLayout(new GridLayout(1, false));
-		FormData fd_chartDisplay_CountByType = new FormData();
-		fd_chartDisplay_CountByType.left = new FormAttachment(tree, 8);
-		fd_chartDisplay_CountByType.right = new FormAttachment(100, -8);
-		fd_chartDisplay_CountByType.top = new FormAttachment(50, 4);
-		fd_chartDisplay_CountByType.bottom = new FormAttachment(100, -8);
-		chartDisplay_CountByType.setLayoutData(fd_chartDisplay_CountByType);
+
+		tbtmNewItem2 = new CTabItem(tabFolder, SWT.NONE);
+		tbtmNewItem2.setText("Count by Path");
+
+		chartDisplay_CountByPath = new Composite(tabFolder, SWT.EMBEDDED);
+		tbtmNewItem2.setControl(chartDisplay_CountByPath);
+		chartDisplay_CountByPath.setLayout(new GridLayout(1, false));
 	}
 
 	@Override
